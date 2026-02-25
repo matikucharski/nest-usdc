157f63e5930ea55d494f4b8447ba166e3d720252
matikucharski <mati.kucharski@gmail.com>
2026-02-25 20:19:36 +0100

    added cache module for caching get endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlockchainModule } from './blockchain/blockchain.module';
@@ -11,9 +12,13 @@ import configuration from './config/configuration';
       load: [configuration],
       isGlobal: true
     }),
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 60 * 1000 // milliseconds
+    }),
     BlockchainModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
