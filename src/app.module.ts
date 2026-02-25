e79c07dd4d3f0175833c25a0d51a3853def8aaca
matikucharski <mati.kucharski@gmail.com>
2026-02-25 20:53:58 +0100

    added rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlockchainModule } from './blockchain/blockchain.module';
@@ -14,11 +16,26 @@ import configuration from './config/configuration';
     }),
     CacheModule.register({
       isGlobal: true,
-      ttl: 60 * 1000 // milliseconds
+      ttl: 60 * 1000 // ms
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        // 3 requests per second
+        {
+          ttl: 1000, // ms
+          limit: 3
+        }
+      ]
     }),
     BlockchainModule
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+    AppService
+  ]
 })
 export class AppModule {}
