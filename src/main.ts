5acc83654eea1b1d2627e3502dcd85d6b13be29d
matikucharski <mati.kucharski@gmail.com>
2026-02-25 19:35:29 +0100

    installed and configured validation packages and installed ethers for handling connection with eth blockchain

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      forbidUnknownValues: true,
+      enableDebugMessages: process.env.NODE_ENV !== 'production'
+    })
+  );
+
+  // MUST be last!
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
