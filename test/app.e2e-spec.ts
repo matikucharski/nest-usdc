c46fc4c5ccdb77b8b62d029d3a832d168bde2b95
matikucharski <mati.kucharski@gmail.com>
2026-02-26 11:54:47 +0100

    added simple test

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,45 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
+
+describe('BlockchainController (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/blockchain/usdc-transfers/13 (GET) - should return empty array with status 200', () => {
+    return request(app.getHttpServer())
+      .get('/blockchain/usdc-transfers/13')
+      .expect(200)
+      .expect('[]');
+  });
+});
