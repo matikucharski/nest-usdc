6231c871279dd0dae67c009558a583b09f1039c4
matikucharski <mati.kucharski@gmail.com>
2026-02-26 01:09:08 +0100

    added controller get endpoint with params validation and cache interceptor

--- src/blockchain/blockchain.controller.ts
+++ src/blockchain/blockchain.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { CacheInterceptor } from '@nestjs/cache-manager';
+import { BlockchainService } from './blockchain.service';
+import { GetUSDCTransfersDto } from './dto/blockchain.input';
+import { TransferInterface } from './interface/blockchain.interface';
 
 @Controller('blockchain')
-export class BlockchainController {}
+@UseInterceptors(CacheInterceptor)
+export class BlockchainController {
+  constructor(private readonly blockchainService: BlockchainService) {}
+
+  @Get('usdc-transfers/:block')
+  getTransfersForUSDC(
+    @Param() params: GetUSDCTransfersDto
+  ): Promise<TransferInterface[]> {
+    return this.blockchainService.getUSDCTransactionsForBlock(params.block);
+  }
+}
